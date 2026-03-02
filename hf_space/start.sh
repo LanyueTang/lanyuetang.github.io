d030b9e90e09a36feef73c7b780f80f0e84888f0
zhuobietly <lanyue.tang@etud.polymtl.ca>
2026-03-02 11:10:26 -0500

    Fix HF Space kernel setup for Voila runtime

--- hf_space/start.sh
+++ hf_space/start.sh
@@ -6,8 +6,12 @@ mkdir -p /app/_paper_notes
 NOTEBOOK_URL="https://raw.githubusercontent.com/zhuobietly/lanyuetang.github.io/HEAD/_paper_notes/stochastic_process_pub.ipynb"
 curl -L "$NOTEBOOK_URL" -o /app/_paper_notes/stochastic_process_pub.ipynb
 
+python -m ipykernel install --name python3 --prefix /usr/local
+
 exec voila /app/_paper_notes/stochastic_process_pub.ipynb \
   --Voila.ip=0.0.0.0 \
   --Voila.port=7860 \
-  --Voila.base_url=/ \
+  --VoilaConfiguration.base_url=/ \
+  --ServerApp.disable_check_xsrf=True \
+  --ServerApp.trust_xheaders=True \
   --no-browser
